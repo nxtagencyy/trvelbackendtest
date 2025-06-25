28e83ca57c62c2133fcb3a7cce81d225193d3ac6
nxtagencyy <cdevnxtagency@gmail.com>
2025-06-25 13:52:44 +0530

    db fix

--- db/db.js
+++ db/db.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load env variables
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://cdevnxtagency:WyMj5N6ozVshVhfk@cluster0.uenjbun.mongodb.net/travelweb?retryWrites=true&w=majority&appName=Cluster0`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ Connected to MongoDB");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error.message);
-    process.exit(1); // Ensures app doesn't run with DB failure
+    process.exit(1);
   }
 };
