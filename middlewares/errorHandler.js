5ef7daf6808b8430112fd2505e1d9a204b334504
nxtagencyy <cdevnxtagency@gmail.com>
2025-06-25 17:33:36 +0530

    deply admin

--- middlewares/errorHandler.js
+++
@@ -1,4 +0,0 @@
-export function errorHandler(err, req, res) {
-  console.error("Error:", err);
-  res.status(500).json({ error: err.message || "Internal Server Error" });
-}
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import express from "express";
 import { connectDB } from "./db/db.js";
-import userRouter from "./routes/user.router.js";
-import authRouter from "./routes/auth.router.js";
 import tourRouter from "./routes/tourPackage.router.js";
 import enquiryRouter from "./routes/enquiry.router.js";
 import adminRouter from "./routes/admin.router.js"; // ✅ Add this
@@ -10,6 +8,9 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+// ✅ Enable CORS for both localhost and production frontend
 app.use(
   cors({
     origin: [
@@ -21,10 +22,23 @@ app.use(
   })
 );
 
-connectDB();
+// app.use(cors({
+//     origin: "http://localhost:3000"
+// }))
 
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/auth", authRouter);
+connectDB();
+// sendEmail({
+//     email: "holeas@krugerschool.edu.ec",
+//     subject: "Prueba",
+//     message: "email de prueba"
+// })
+//     .then(()=>{
+//         console.log('Email enviado')
+//     })
+//     .catch((error)=> {
+//         console.error(error);
+//     })
+
 app.use("/api/tours", tourRouter);
 app.use("/api/enquiry", enquiryRouter);
 app.use("/api/admin", adminRouter); // ✅ Mount admin routes
